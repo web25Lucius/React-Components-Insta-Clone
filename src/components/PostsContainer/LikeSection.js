fc44a6436fe2e75ca29daf612597b47c89bed553
web25Lucius <web25Lucius@gmail.com>
2020-03-02 15:52:29 -0700

    mapping postpage

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,9 +1,10 @@
 // look at the likes on this component. Right now it is hard coded on line 20.
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
-import React from "react";
+import React, {useState} from "react";
 
 const LikeSection = props => {
+  const [like, setLike] = useState(0); 
   return (
     <div>
     <div
@@ -17,9 +18,7 @@ const LikeSection = props => {
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">
-      
-      likes</p>
+    <p className="like-number">{setLike}</p>
 </div>
   )
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,14 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from "../../dummy-data";
 // import data 
-
-const PostsPage = () => {
+const PostsPage = (prop) => {
   // set up state for your data
+const [state, setState] = useState (""); 
   return (
     <div className="posts-container-wrapper">
+      {dummyData.map ((item)=> {
+        return <div className="post-border">
+                <p className="post-text" postText={item.prop}/>
+                </div>
+      })}
       {/* map through data here to return a Post and pass data as props to Post */}
     </div>
   );
